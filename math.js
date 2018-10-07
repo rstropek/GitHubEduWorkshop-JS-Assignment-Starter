df65c1801047c12b3eb790b46ba51031e1268d62
Rainer Stropek <rainer@software-architects.at>
2018-10-07 16:56:28 +0200

    Add starter code

---
+++ math.js
@@ -0,0 +1,15 @@
+exports.add = function (x, y) {
+    // ToDo: Add the necessary code to add two numbers
+    return NaN;
+}
+
+exports.sub = function (x, y) {
+    // ToDo: Add the necessary code to subtract two numbers
+    return NaN;
+}
+
+exports.sumOfPositiveNumbers = function (upperLimitInclusive) {
+    // ToDo: Add the necessary code to calculate the sum of all positive
+    //       numbers lower equal the specified upper limit.
+    return NaN;
+}
